1792f6f0c3f624d0866d6c2c84fcb789a60082f3
namanUIUC <namanatulshukla@gmail.com>
2018-04-23 15:17:00 -0500

    added io_tools

--- io_tools.py
+++ io_tools.py
@@ -14,7 +14,7 @@ def _parse_image(path):
     img = tf.image.random_flip_left_right(img)
     img = tf.image.resize_images(img, [load_size, load_size])
     img = (img - tf.reduce_min(img)) / (tf.reduce_max(img) - tf.reduce_min(img))
-    img = tf.random_crop(img, [crop_size, crop_size, channels])
+    img = tf.random_crop(img, [crop_size, crop_size, 3])
     img = img * 2 - 1
     return img
 
