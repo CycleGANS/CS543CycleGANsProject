df1cc0d23a1844e4de80b9695266a2685261f958
Harshad Rai <hrrai2@illinois.edu>
2018-04-23 09:03:47 -0500

    mainErrorRemoval

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import utils
 import Generator as gen
 import simple_discriminator as dis
 
@@ -43,15 +42,17 @@ D_GofXlogits = dis.build_gen_discriminator(GofX, scope = 'DY')
 # https://arxiv.org/pdf/1611.04076.pdf this paper states that using cross entropy as loss
 # causes the gradient to vanish. To avoid this problem, least squares losses are used as suggested by the paper.
 
-# Losses for G
+# Adversary and Cycle Losses for G
 G_adv_loss = tf.reduce_mean(tf.squared_difference(D_GofXlogits, tf.ones_like(D_GofXlogits)))
-G_cyc_loss = tf.reduce_mean(tf.abs(GofFofY-Y))
-G_tot_loss = G_adv_loss + G_cyc_loss_weight * G_cyc_loss       # Put lambda for G cyclic loss here
+G_cyc_loss = tf.reduce_mean(tf.abs(GofFofY-Y)) * G_cyc_loss_weight        # Put lambda for G cyclic loss here
 
-#Losses for F
+# Adversary and Cycle Losses for F
 F_adv_loss = tf.reduce_mean(tf.squared_difference(D_FofYlogits,tf.ones_like(D_FofYlogits)))
-F_cyc_loss = tf.reduce_mean(tf.abs(FofGofX-X))
-F_tot_loss = F_adv_loss + F_cyc_loss_weight * F_cyc_loss       # Put lambda for F cyclic loss here
+F_cyc_loss = tf.reduce_mean(tf.abs(FofGofX-X)) * F_cyc_loss_weight        # Put lambda for F cyclic loss here
+
+# Total Losses for G and F
+G_tot_loss = G_adv_loss + G_cyc_loss + F_cyc_loss
+F_tot_loss = F_adv_loss + F_cyc_loss + G_cyc_loss
 
 # Losses for DX
 DX_real_loss = tf.reduce_mean(tf.squared_difference(D_Xlogits, tf.ones_like(D_Xlogits)))
@@ -80,29 +81,7 @@ DY_train_step = optimizer.minimize(DY_tot_loss, var_list = DY_variables)
 
 
 # Summary for Tensor Board
-G_summary = utils.summary({G_adv_loss: 'G_adv_loss',
-                           G_cyc_loss: 'G_cyc_loss',
-                           G_tot_loss: 'G_tot_loss'})
-F_summary = utils.summary({F_adv_loss: 'F_adv_loss',
-                           F_cyc_loss: 'F_cyc_loss',
-                           F_tot_loss: 'F_tot_loss'})
-DX_summary = utils.summary({DX_real_loss: 'DX_real_loss',
-							DX_fake_loss: 'DX_fake_loss',
-							DX_tot_loss: 'DX_tot_loss'})
-DY_summary = utils.summary({DY_real_loss: 'DY_real_loss',
-							DY_fake_loss: 'DY_fake_loss',
-							DY_tot_loss: 'DY_tot_loss'})
-
-# Summary for Tensor Board
-G_summary = utils.summary({G_adv_loss: 'G_adv_loss',
-                           G_cyc_loss: 'G_cyc_loss',
-                           G_tot_loss: 'G_tot_loss'})
-F_summary = utils.summary({F_adv_loss: 'F_adv_loss',
-                           F_cyc_loss: 'F_cyc_loss',
-                           F_tot_loss: 'F_tot_loss'})
-DX_summary = utils.summary({DX_real_loss: 'DX_real_loss',
-							DX_fake_loss: 'DX_fake_loss',
-							DX_tot_loss: 'DX_tot_loss'})
-DY_summary = utils.summary({DY_real_loss: 'DY_real_loss',
-							DY_fake_loss: 'DY_fake_loss',
-							DY_tot_loss: 'DY_tot_loss'})
+G_summary = tf.summary.scalar("G_tot_loss", G_tot_loss)
+F_summary = tf.summary.scalar("F_tot_loss", F_tot_loss)
+DX_summary = tf.summary.scalar("DX_tot_loss", DX_tot_loss)
+DY_summary = tf.summary.scalar("DY_tot_loss", DY_tot_loss)
